91fd0c040a6513260a45efd1e8a0591205b4aee1
ivaylo-traykov <ivailotraikov@abv.bg>
2024-09-22 14:11:08 +0300

    Added playing card models and card state machine with drag and drop functionallity

---
+++ scenes/card_ui/card_states/card_clicked_state.gd
@@ -0,0 +1,29 @@
+extends CardState
+
+
+func enter() -> void:
+	card_ui.color.color = Color.ORANGE
+	card_ui.state.text = "CLICKED"
+	card_ui.drop_point_detector.monitoring = true
+
+
+func exit() -> void:
+	pass
+
+
+func on_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		transition_requested.emit(self, CardState.State.DRAGGING)
+
+
+func on_gui_input(event: InputEvent) -> void:
+	pass
+
+
+func on_mouse_entered() -> void:
+	pass
+
+
+func on_mouse_exited() -> void:
+	pass
+
---
+++ scenes/card_ui/card_states/card_released_state.gd
@@ -0,0 +1,38 @@
+extends CardState
+
+
+var played: bool
+
+
+func enter() -> void:
+	card_ui.color.color = Color.DARK_VIOLET
+	card_ui.state.text = "RELEASED"
+	
+	played = false
+	
+	if not card_ui.targets.is_empty():
+		played = true
+		print("play card for targets: ", card_ui.targets)
+
+func exit() -> void:
+	pass
+
+
+func on_input(event: InputEvent) -> void:
+	if played:
+		return
+		
+	transition_requested.emit(self, CardState.State.BASE)
+
+
+func on_gui_input(event: InputEvent) -> void:
+	pass
+
+
+func on_mouse_entered() -> void:
+	pass
+
+
+func on_mouse_exited() -> void:
+	pass
+
